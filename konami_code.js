380838ab3bc582f7dca30958793514bee1194342
Michelle Sharpe <hidibyms@gmail.com>
2019-11-12 20:34:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -18,19 +18,18 @@ document.body.eventListener('keydown', init)
 
 function init(e) {
     const key = e.key;
-    if (key)
-}
+    if (key === codes[index]){
+      index++;
+      if (index === codes.length){
+        alert("Hurray!");
+        index = 0; 
+      }
+    } else {
+      index = 0;
+    }
+  }
 
 
 
-// {
-// // your code here - attach event listener to document.body and check for 'keydown' events
-// const key = e.key;
-// 
 
-
-//   // if user presses all 10 keys in correct order alert() congratulations
-//   // if not pressed correctly no alert keep listening for ten keydown in correct order
-
-// }
 
