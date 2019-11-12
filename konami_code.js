98fcf859b1eb560aa67a0ab501d21ca766a054e5
Michelle Sharpe <hidibyms@gmail.com>
2019-11-12 20:28:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,27 +15,29 @@ const codes = [
 // keep track of index outside of event handler
 let index = 0;
 
-function init("keydown", function(event)) {}
-
-
-
-
-{
-// your code here - attach event listener to document.body and check for 'keydown' events
-const key = e.key;
-if (key === codes[index]){
-  index++;
-  if(index === codes.length){
-    alert("Hurray!");
-    index = 0;
-  }
-} else {
-  index = 0;
+function init() {
+  
 }
+document.body.eventListener
 
 
-  // if user presses all 10 keys in correct order alert() congratulations
-  // if not pressed correctly no alert keep listening for ten keydown in correct order
 
-}
+// {
+// // your code here - attach event listener to document.body and check for 'keydown' events
+// const key = e.key;
+// if (key === codes[index]){
+//   index++;
+//   if(index === codes.length){
+//     alert("Hurray!");
+//     index = 0;
+//   }
+// } else {
+//   index = 0;
+// }
+
+
+//   // if user presses all 10 keys in correct order alert() congratulations
+//   // if not pressed correctly no alert keep listening for ten keydown in correct order
+
+// }
 
